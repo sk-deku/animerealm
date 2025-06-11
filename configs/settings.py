8cf22efab800a9d54f7676136fc86604964722de
sk-deku <kesavansivakumar50@gmail.com>
2025-06-11 19:23:36 +0530

    Update settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -21,7 +21,7 @@ logging.getLogger("telegram.ext").setLevel(logging.INFO)
 
 # --- Core Bot Settings ---
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "").split(',') if admin_id]
+ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "1775977570").split(',') if admin_id]
 BOT_USERNAME = os.getenv("BOT_USERNAME") # e.g., "MyAnimeRealmBot" - used for t.me links
 DEFAULT_PARSE_MODE = "HTML"
 DEBUG_MODE = os.getenv("DEBUG_MODE", "True").lower() == "true"
