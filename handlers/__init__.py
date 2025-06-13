c31a6518a6093a9726dfc040b933752c114240d5
sk-deku <kesavansivakumar50@gmail.com>
2025-06-13 15:46:31 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,17 +1,11 @@
-# handlers/__init__.py
-# Pyrogram loads all modules imported in this file as plugins
-# Add any handler modules you create here
-
 from . import common_handlers
-from . import admin_handlers
-from . import browse_handler
-from . import search_handler
-from . import download_handler
-from . import request_handler
-from . import content_handler
-from . import watchlist_handler
-from . import tokens_handler
-from . import callback_handlers
+from . import tokens_handler # Uncommented if tokens_handler is complete enough
+from . import content_handler # <-- Uncomment this!
+# from . import browse_handler
+# from . import search_handler
+# from . import download_handler
+# from . import request_handler
+# from . import watchlist_handler
+# from . import premium_handler
+# from . import callback_handlers # This might be combined/integrated
 
-# Note: We'll uncomment these imports as we create the respective files.
-# For now, only common_handlers is needed to start.
