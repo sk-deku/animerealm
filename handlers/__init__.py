c2ddeeabf41799bd392bbfd87558a6ddeb35b4ef
sk-deku <kesavansivakumar50@gmail.com>
2025-06-13 13:18:31 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,14 @@
-
+# handlers/__init__.py
+# List the names of the files containing handlers to be loaded
+__all__ = [
+    "common_handlers",
+    "admin_handlers", # Will uncomment/add as we create them
+    "browse_handler",
+    "search_handler",
+    "download_handler",
+    "request_handler",
+    "watchlist_handler",
+    "tokens_handler",
+    "callback_handlers",
+    "content_handler", # For admin manage_content
+]
