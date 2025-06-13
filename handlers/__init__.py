1768207b1e742fe0717c00d353b63d633c665c08
sk-deku <kesavansivakumar50@gmail.com>
2025-06-13 16:59:26 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,11 @@
 from . import common_handlers
-from . import tokens_handler # Uncommented if tokens_handler is complete enough
-from . import content_handler # <-- Uncomment this!
+from . import tokens_handler
+from . import content_handler 
 from . import browse_handler
 from . import search_handler
 from . import download_handler
 from . import request_handler
 from . import watchlist_handler
 from . import premium_handler
-from . import callback_handlers # This might be combined/integrated
-
+from . import callback_handlers
+from . import admin_handlers  
