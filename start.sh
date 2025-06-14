262a25911155790e28b5fbb2043db37f65013e26
sk-deku <kesavansivakumar50@gmail.com>
2025-06-14 10:31:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo ...."
-git clone https://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@github.com/sk-deku/animerealm /animerealm
+git clone https://github.com/sk-deku/animerealm /animerealm
 cd /animerealm
 pip3 install r-requirements.txt
 echo "Starting bot...."
